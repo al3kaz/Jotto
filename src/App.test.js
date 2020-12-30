c0c4fe70cc8f21a3779f1dc3994eaa16dc672350
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2020-12-30 21:50:10 +0100

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+test('renders button', () => {});
+test('renders counter', () => {});
+test('counter start at 0', () => {});
+test('CLICKING ON BUTTON INCREMENT COUNTER DISPLAY', () => {});
