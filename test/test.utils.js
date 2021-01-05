8d8ec30c03ee099d56008f0d9d80ac61bcf3572d
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2021-01-05 20:46:35 +0100

    install Redux Thunk middleware

--- test/test.utils.js
+++ test/test.utils.js
@@ -1,13 +1,17 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../src/redux/root-reducer';
+import { middlewares } from '../src/redux/store';
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`);
 };
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 export const checkProps = (component, conformingProp) => {
