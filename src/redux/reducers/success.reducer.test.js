ee92a5b91a0f58d073b06e69692872750e60d018
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2021-01-05 10:30:03 +0100

    create CORRECT_GUESS action and test, write code and tests for success.reducer

---
+++ src/redux/reducers/success.reducer.test.js
@@ -0,0 +1,13 @@
+import { actionTypes } from '../actions/index';
+import successReducer from './success.reducer';
+
+test('returns default initial state of "false" when no action is passed', () => {
+  const newState = successReducer(undefined, {});
+  expect(newState).toBe(false);
+});
+test('return state of "true" when receiving an action of type "CORRECT_GUESS"', () => {
+  const newState = successReducer(undefined, {
+    type: actionTypes.CORRECT_GUESS,
+  });
+  expect(newState).toBe(true);
+});
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import successReducer from './success/success.reducer';
+import successReducer from './reducers/success.reducer';
 
 export default combineReducers({
   success: successReducer,
