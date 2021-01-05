cfdb83c88f8871e228e0dfaf5fbe46c3c7f28419
Aleksander Kazmierczak <aleksander.kazmierczak@gmail.com>
2021-01-05 16:10:12 +0100

    tests for Input success=true

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import Congrats from '../Congrats/Congrats.component'
 
 class Input extends Component {
    constructor(props) {
